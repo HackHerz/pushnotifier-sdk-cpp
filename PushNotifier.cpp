74ab257797056cdeef7ebb11ead11e773008d118
Daniel Stein <danielmarianstein@yahoo.com>
2018-01-10 00:25:36 +0100

    Everything implemented. Documentation is just a copy of the PHP SDK and therefore not correct

--- PushNotifier.cpp
+++ PushNotifier.cpp
@@ -7,11 +7,13 @@ using namespace std;
 using json = nlohmann::json;
 
 
+
 PushNotifier::PushNotifier() {
 	this->authorization = base64::encode((string) APP_PACKAGE + ":" + API_TOKEN);
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 }
+
 
 
 PushNotifier::~PushNotifier() {
@@ -29,16 +31,7 @@ PushNotifier::PushNotifier(string username, string appToken, time_t expires_at) 
 }
 
 
-
-// needed for handling curl output¬
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-	((string*) userp)->append((char*) contents, size * nmemb);
-	return size * nmemb;
-}
-
-
-
-string PushNotifier::login(string username, string password, bool apply) {
+PushNotifier::AppToken PushNotifier::login(string username, string password, bool apply) {
 	json j;
 	j["username"] = username;
 	j["password"] = password;
@@ -49,11 +42,15 @@ string PushNotifier::login(string username, string password, bool apply) {
 		throw runtime_error(buf["message"].dump());
 	}
 
+	AppToken tok;
+	tok.token = buf["app_token"];
+	tok.expires_at = buf["expires_at"].get<time_t>();
+
 	if(apply) {
-		this->appToken.token = buf["app_token"];
+		this->appToken = tok;
 	}
 
-	return buf["app_token"];
+	return tok;
 }
 
 
@@ -81,6 +78,130 @@ vector<PushNotifier::Device>PushNotifier::getDevices() {
 
 
 
+PushNotifier::AppToken PushNotifier::refreshToken(bool apply) {
+	json j = call(GET, URL_PN_REFRESH, this->appToken.token.c_str(), "{}"_json);
+
+	if(j["status"] == "error") {
+		throw runtime_error(j["message"].dump());
+	}
+
+	AppToken tk;
+	tk.token = j["app_token"];
+	tk.expires_at = j["expires_at"].get<time_t>();
+
+	if(apply) {
+		this->appToken = tk;
+	}
+
+	return tk;
+}
+
+
+
+void PushNotifier::setApptoken(string appToken, time_t expires_at) {
+	this->appToken.token = appToken;
+	this->appToken.expires_at = expires_at;
+}
+
+
+
+string PushNotifier::getAppToken() {
+	if(this->appToken.isAboutToExpire()) {
+		refreshToken();
+	}
+
+	return this->appToken.token;
+}
+
+
+
+bool PushNotifier::sendMessage(vector<string> deviceIDs, string content, bool silent) {
+	if(content.empty()) {
+		throw new runtime_error("Content shall not be empty");
+	}
+
+	json request;
+	request["devices"] = deviceIDs;
+	request["content"] = content;
+	request["silent"] = silent;
+
+	json j = call(PUT, URL_PN_TXT, this->appToken.token.c_str(), request);
+
+	return j["error"].empty();
+}
+
+
+bool PushNotifier::sendMessage(string deviceID, string content, bool silent) {
+	vector<string> buf;
+	buf.push_back(deviceID);
+
+	return sendMessage(buf, content, silent);
+}
+
+
+
+
+
+bool PushNotifier::sendURL(vector<string> deviceIDs, string url, bool silent) {
+	if(url.empty()) {
+		throw new runtime_error("URL shall not be empty");
+	}
+
+	json request;
+	request["devices"] = deviceIDs;
+	request["url"] = url;
+	request["silent"] = silent;
+
+	json j = call(PUT, URL_PN_URL, this->appToken.token.c_str(), request);
+
+	return j["error"].empty();
+}
+
+
+bool PushNotifier::sendURL(string deviceID, string url, bool silent) {
+	vector<string> buf;
+	buf.push_back(deviceID);
+
+	return sendURL(buf, url, silent);
+}
+
+
+
+
+
+bool PushNotifier::sendNotification(vector<string> deviceIDs, string content, string url, bool silent) {
+	if(url.empty() || content.empty()) {
+		throw new runtime_error("Content and URL shall not be empty");
+	}
+
+	json request;
+	request["devices"] = deviceIDs;
+	request["url"] = url;
+	request["content"] = content;
+	request["silent"] = silent;
+
+	json j = call(PUT, URL_PN_NOTIFICATION, this->appToken.token.c_str(), request);
+
+	return j["error"].empty();
+}
+
+
+bool PushNotifier::sendNotification(string deviceID, string content, string url, bool silent) {
+	vector<string> buf;
+	buf.push_back(deviceID);
+
+	return sendNotification(buf, content, url, silent);
+}
+
+
+
+
+// needed for handling curl output¬
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	((string*) userp)->append((char*) contents, size * nmemb);
+	return size * nmemb;
+}
+
 
 
 json PushNotifier::call(http_method method, string url, const char* xapptoken, json body) {
@@ -92,7 +213,7 @@ json PushNotifier::call(http_method method, string url, const char* xapptoken, j
 
 	// We need to add an authentication header
 	string auth = "Authorization: Basic " + this->authorization;
-	chunk = curl_slist_append(chunk, auth.c_str()); // TODO
+	chunk = curl_slist_append(chunk, auth.c_str());
 	chunk = curl_slist_append(chunk, "Accept: application/json");
 	chunk = curl_slist_append(chunk, "Content-Type: application/json");
 
@@ -105,15 +226,18 @@ json PushNotifier::call(http_method method, string url, const char* xapptoken, j
 	curl = curl_easy_init();
 
 	if(curl) {
+		string data = body.dump();
+
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
 		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
-		string data = body.dump();
 
 		if(method == POST) {
 			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+		} else if(method == PUT) {
+			curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
 		}
-
 
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -134,11 +258,9 @@ json PushNotifier::call(http_method method, string url, const char* xapptoken, j
 			throw runtime_error("");
 		}
 
-
 		// always cleanup
 		curl_easy_cleanup(curl);
 	}
-
 
 	return json::parse(readBuffer);
 }
--- PushNotifier.h
+++ PushNotifier.h
@@ -19,53 +19,219 @@
 // URLs
 #define URL_PN_LOGIN		"https://api.pushnotifier.de/v2/user/login"
 #define URL_PN_DEVICES		"https://api.pushnotifier.de/v2/devices"
-
-/*
- * https://api.pushnotifier.de/v2/user/refresh
- * https://api.pushnotifier.de/v2/notifications/text
- * https://api.pushnotifier.de/v2/notifications/url
- * https://api.pushnotifier.de/v2/notifications/notification
- */
+#define URL_PN_REFRESH		"https://api.pushnotifier.de/v2/user/refresh"
+#define URL_PN_TXT			"https://api.pushnotifier.de/v2/notifications/text"
+#define URL_PN_URL			"https://api.pushnotifier.de/v2/notifications/url"
+#define URL_PN_NOTIFICATION	"https://api.pushnotifier.de/v2/notifications/notification"
 
 
 
-
+/**
+ * PushNotifier SDK Service
+ * A bridge for using PushNotifier
+ *
+ * @author hackherz
+ * @version 1.0 <January 2018>
+*/
 class PushNotifier
 {
 public:
+
+	/**
+	 * Initialize the PushNotifier instance
+	 */
 	PushNotifier();
+
+
+	/**
+	 * Initialize the PushNotifier instance
+	 * Takes a config array with these options:
+	 *
+	 * @param TODO
+	 */
 	PushNotifier(std::string username, std::string appToken, std::time_t expires_at);
 
 	~PushNotifier();
 
 
+	/**
+	 * A PushNotifier device
+	 *
+	 * TODO
+	 */
 	typedef struct {
 		std::string title;
 		std::string id;
 		std::string model;
 		std::string image;
+
+		std::string toString() {
+			return "Title: " + title + " ID: " + id + " Model: " + model;
+		}
 	} Device;
 
+
+	/**
+	 * Represents an AppToken
+	 *
+	 * TODO
+	 */
 	typedef struct {
 		std::string token;
 		std::time_t expires_at;
+
+		bool isAboutToExpire() {
+			return std::difftime(expires_at, std::time(nullptr)) < 48.0 * 3600;
+		}
 	} AppToken;
 
-	std::string login(std::string username, std::string password, bool apply = true);
+
+	/**
+	 * Log in as a user
+	 *
+	 * @param string       $username Username
+	 * @param string       $password Password
+	 * @param bool         $apply    Automatically apply the AppToken to this instance
+	 *
+	 * @return AppToken
+	 *
+	 * @throws InvalidCredentialsException If the users couldn't be authenticated
+	 * @throws PushNotifierException If an unknown error happened
+	 */
+	AppToken login(std::string username, std::string password, bool apply = true);
+
+
+	/**
+	 * Refresh the current AppToken
+	 * This can be done any time but shouldn't be done until 48 hours before it expires
+	 *
+	 * @param bool apply Automatically apply the AppToken to this instance
+	 *
+	 * @return AppToken
+	 *
+	 * @throws InvalidAppTokenException If the token had expired already
+	 * @throws PushNotifierException If an unknown error happened
+	 */
+	AppToken refreshToken(bool apply = true);
+
+
+	/**
+	 * Get all devices the current user has
+	 *
+	 * @return Device[]
+	 *
+	 * @throws InvalidAppTokenException If the AppToken has expired or is invalid
+	 * @throws PushNotifierException If an unknown error happened
+	 */
 	std::vector<Device> getDevices();
 
 
+	/**
+	 * Set the AppToken that should be used
+	 *
+	 * @param string        $appToken AppToken
+	 * @param DateTime|null $expiry   Expiration Date of the AppToken, set null to disable auto-renewal
+	 *
+	 * @return PushNotifier
+	 */
+	void setApptoken(std::string appToken, std::time_t expires_at);
+
+
+	/**
+	 * Get the current AppToken
+	 * Renews itself automatically in case it is about to expire
+	 *
+	 * @return string
+	 */
+	std::string getAppToken();
+
+
+
+	/**
+	 * Send a text-only message
+	 *
+	 * @param array        $devices Devices to send to, either Device[] or string[] (IDs of the devices)
+	 * @param string       $content Content to send
+	 * @param bool         $silent  If true, no sound is played
+	 *
+	 * @return array                 [ 'success' => [ { device_id: ... }, ... ], 'error' => [ { device_id: ... }, ... ] ]
+	 *
+	 * @throws DeviceNotFoundException If at least one device could not be found
+	 * @throws InvalidAppTokenException If the AppToken has expired or is invalid
+	 * @throws PushNotifierException If an unknown error happened
+	 */
+	bool sendMessage(std::vector<std::string> deviceIDs, std::string content, bool silent = false);
+
+
+	/// TODO
+	bool sendMessage(std::string deviceID, std::string content, bool silent = false);
+
+
+	/**
+	 * Send a link
+	 *
+	 * @param array        $devices Devices to send to, either Device[] or string[] (IDs of the devices)
+	 * @param string       $url     URL to send
+	 * @param bool         $silent  If true, no sound is played
+	 *
+	 * @return array                 [ 'success' => [ { device_id: ... }, ... ], 'error' => [ { device_id: ... }, ... ] ]
+	 *
+	 * @throws DeviceNotFoundException If at least one device could not be found
+	 * @throws InvalidAppTokenException If the AppToken has expired or is invalid
+	 * @throws PushNotifierException If an unknown error happened
+	 */
+	bool sendURL(std::vector<std::string> deviceIDs, std::string url, bool silent = false);
+
+
+	/// TODO
+	bool sendURL(std::string deviceID, std::string url, bool silent = false);
+
+
+	/**
+	 * Send a notification with content and a URL
+	 * The user will be taken to the URL after tapping on the notification
+	 *
+	 * @param array        $devices Devices to send to, either Device[] or string[] (IDs of the devices)
+	 * @param string       $content Content to send
+	 * @param string       $url     URL to send
+	 * @param bool         $silent  If true, no sound is played
+	 *
+	 * @return array                 [ 'success' => [ { device_id: ... }, ... ], 'error' => [ { device_id: ... }, ... ] ]
+	 *
+	 * @throws DeviceNotFoundException If at least one device could not be found
+	 * @throws InvalidAppTokenException If the AppToken has expired or is invalid
+	 * @throws PushNotifierException If an unknown error happened
+	 */
+	bool sendNotification(std::vector<std::string> deviceIDs, std::string content, std::string url, bool silent = false);
+
+
+	/// TODO
+	bool sendNotification(std::string deviceID, std::string content, std::string url, bool silent = false);
+
+
+
 private:
+	/**
+	 * TODO
+	 */
 	typedef enum {
 		PUT,
 		GET,
 		POST
 	} http_method;
 
+	/**
+	 * TODO
+	 */
 	nlohmann::json call(http_method method, std::string url, const char* xapptoken, nlohmann::json body);
 
+	/// TODO
 	std::string username;
+
+	/// TODO
 	AppToken appToken;
+
+	/// TODO
 	std::string authorization;
 };
 
